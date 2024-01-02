07bf473ac584bb75ded0f275aed8f6bcbdc0bcbf
misaku54 <eisei0504@outlook.jp>
2024-01-02 17:37:41 +0900

    [update]chapter16

---
+++ if_statement.php
@@ -0,0 +1,16 @@
+<?php
+$height = 90;
+var_dump($height);
+
+// 左比較したい変数
+if ($height == 90){
+  echo '身長は' . $height . 'cmです';
+}
+
+if ($height === '90'){
+  echo '身長は' . $height . 'cmです';
+}
+
+// == 一致
+// === 型も一致
+?>
